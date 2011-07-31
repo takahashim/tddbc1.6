c2711e50108ed777c44d40400a583280a8368e45
takahashim <maki@rubycolor.org>
2011-07-31 14:57:35 +0900

    contextとdescribeの順番を変更

--- kv_spec.rb
+++ kv_spec.rb
@@ -11,28 +11,35 @@ describe KV do
     before do
       @kv.put("k", "v")
     end
-
-    it "getでkeyに対応するvalueを取得できる" do
-      @kv.get("k").should == "v"
+    describe "#get" do
+      it "getでkeyに対応するvalueを取得できる" do
+        @kv.get("k").should == "v"
+      end
     end
 
-    it "dumpで一覧表示" do
-      @kv.dump_string.should == "k: v\n"
+    describe "#put" do
+      it "putの引数に既に存在するkeyが指定された場合、valueのみを更新する" do
+        @kv.put("k", "v2")
+        @kv.get("k").should == "v2"
+      end
     end
 
-    it "deleteで指定したkeyとvalueを削除する" do
-      @kv.delete("k")
-      @kv.get("k").should == nil
+    describe "#dump" do
+      it "dumpで一覧表示" do
+        @kv.dump_string.should == "k: v\n"
+      end
     end
 
-    it "kが削除されていること" do
-      @kv.delete("k")
-      @kv.dump_string.should == ""
-    end
+    describe "#delete" do
+      it "deleteで指定したkeyとvalueを削除する" do
+        @kv.delete("k")
+        @kv.get("k").should == nil
+      end
 
-    it "putの引数に既に存在するkeyが指定された場合、valueのみを更新する" do
-      @kv.put("k", "v2")
-      @kv.get("k").should == "v2"
+      it "kが削除されていること" do
+        @kv.delete("k")
+        @kv.dump_string.should == ""
+      end
     end
   end
 
@@ -41,19 +48,26 @@ describe KV do
       @kv.put("k", "v")
       @kv.put("k2", "v2")
     end
-    it "dumpで一覧表示" do
-      @kv.dump_string.should == "k: v\nk2: v2\n"
+    describe "#dump" do
+      it "dumpで一覧表示" do
+        @kv.dump_string.should == "k: v\nk2: v2\n"
+      end
     end
   end
 
-  it "keyとvalueのセットを一度に複数追加できること" do
-    @kv.mput(["k", "k2"], ["v", "v2"])
-    @kv.get("k").should == "v"
-    @kv.get("k2").should == "v2"
-  end
+  describe "#mput" do
+    context "引数の個数がそれぞれ2個の場合" do
+      it "keyとvalueのセットを一度に複数追加できること" do
+        @kv.mput(["k", "k2"], ["v", "v2"])
+        @kv.get("k").should == "v"
+        @kv.get("k2").should == "v2"
+      end
+    end
 
-  it "個数が違う場合例外が発生すること" do
-    expect { @kv.mput(["k", "k2"], ["v", "v2", "v3"]) }.to raise_error
+    context "引数の個数が違う場合" do
+      it "個数が違う場合例外が発生すること" do
+        expect { @kv.mput(["k", "k2"], ["v", "v2", "v3"]) }.to raise_error
+      end
+    end
   end
-
 end
