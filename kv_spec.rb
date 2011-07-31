e65d54e903ac8c90ed0a46a36854c87487567b6b
takahashim <maki@rubycolor.org>
2011-07-31 14:42:26 +0900

    テストケースの見た目をちょっとだけ綺麗にした

--- kv_spec.rb
+++ kv_spec.rb
@@ -2,48 +2,48 @@
 require './kv'
 require 'rspec'
 
-describe "kv" do
+describe KV do
   before do
     @kv = KV.new
   end
 
-  it "getでkeyに対応するvalueを取得できる" do
-    @kv.put("k", "v")
-    @kv.get("k").should == "v"
+  context "keyが'k'でvalueが'v'のペアが入っている場合" do
+    before do
+      @kv.put("k", "v")
+    end
+
+    it "getでkeyに対応するvalueを取得できる" do
+      @kv.get("k").should == "v"
+    end
+
+    it "dumpで一覧表示" do
+      @kv.dump_string.should == "k: v\n"
+    end
+
+    it "deleteで指定したkeyとvalueを削除する" do
+      @kv.delete("k")
+      @kv.get("k").should == nil
+    end
+
+    it "kが削除されていること" do
+      @kv.delete("k")
+      @kv.dump_string.should == ""
+    end
+
+    it "putの引数に既に存在するkeyが指定された場合、valueのみを更新する" do
+      @kv.put("k", "v2")
+      @kv.get("k").should == "v2"
+    end
   end
 
-  it "putでkeyとvalueを追加する" do
-    @kv.put("k2", "v2")
-    @kv.get("k2").should == "v2"
-  end
-
-  it "dumpで一覧表示" do
-    @kv.put("k", "v")
-    @kv.dump_string.should == "k: v\n"
-  end
-
-  it "複数個値を持っている場合、dumpで一覧表示" do
-    @kv.put("k", "v")
-    @kv.put("k2", "v2")
-    @kv.dump_string.should == "k: v\nk2: v2\n"
-  end
-
-  it "deleteで指定したkeyとvalueを削除する" do
-    @kv.put("k", "v")
-    @kv.delete("k")
-    @kv.get("k").should == nil
-  end
-
-  it "kが削除されていること" do
-    @kv.put("k", "v")
-    @kv.delete("k")
-    @kv.dump_string.should == ""
-  end
-
-  it "putの引数に既に存在するkeyが指定された場合、valueのみを更新する" do
-    @kv.put("k", "v")
-    @kv.put("k", "v2")
-    @kv.get("k").should == "v2"
+  context "複数個値を持っている場合" do
+    before do
+      @kv.put("k", "v")
+      @kv.put("k2", "v2")
+    end
+    it "dumpで一覧表示" do
+      @kv.dump_string.should == "k: v\nk2: v2\n"
+    end
   end
 
   it "keyとvalueのセットを一度に複数追加できること" do
